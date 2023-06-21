0cbc9349461e25a0b70135e5191f79e0f94285ad
Minsu Kim <syt07100@gmail.com>
2023-06-21 23:17:52 +0900

    first commit

---
+++ src/test/java/porori/backend/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package porori.backend.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
